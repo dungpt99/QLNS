6f73a748c9146db2180a4f1f2760c75265aff7de
dungpt99 <phungdung080299@gmail.com>
2021-10-18 21:27:09 +0700

    report

--- src/modules/report/report.controller.js
+++ src/modules/report/report.controller.js
@@ -1,26 +1,82 @@
 const { Op } = require('sequelize')
-const { reports, forms } = require('../model')
+const { users, reports, forms } = require('../model')
 
 class ReportController {
-  //GET
-  async job(req, res, next) {
+  //CATCH data
+  async dataJob(req, res, next) {
+    await reports.destroy({
+      truncate: true,
+    })
     const listForm = await forms.findAll({
       where: {
         status: { [Op.ne]: 'closed' },
+        type: 'job',
         createdAt: {
-          [Op.lt]: new Date(),
-          [Op.gt]: new Date(new Date() - 24 * 60 * 60 * 1000),
+          [Op.lt]: new Date(new Date() - 24 * 60 * 60 * 1000),
         },
       },
     })
-    const result = new Date(listForm[0].createdAt.getTime() + 24 * 60 * 60 * 1000)
+    for (const item of listForm) {
+      const user = await users.findByPk(item.userId)
+      await reports.create({
+        formId: item.id,
+        fname: user.fname,
+        lname: user.lname,
+        status: item.status,
+        type: item.type,
+        expired: new Date(item.createdAt.getTime() + 24 * 60 * 60 * 1000),
+      })
+    }
+    next()
+  }
+
+  //CATCH data
+  async dataReview(req, res, next) {
+    await reports.destroy({
+      truncate: true,
+    })
+    const listForm = await forms.findAll({
+      where: {
+        status: { [Op.ne]: 'closed' },
+        type: 'review',
+        createdAt: {
+          [Op.lt]: new Date(new Date() - 24 * 60 * 60 * 1000),
+        },
+      },
+    })
+    for (const item of listForm) {
+      const user = await users.findByPk(item.userId)
+      await reports.create({
+        formId: item.id,
+        fname: user.fname,
+        lname: user.lname,
+        status: item.status,
+        type: item.type,
+        expired: new Date(item.createdAt.getTime() + 24 * 60 * 60 * 1000),
+      })
+    }
+    next()
+  }
+
+  //GET
+  async job(req, res, next) {
+    const listReport = await reports.findAll()
     res.status(200).json({
-      result,
+      listReport,
     })
   }
 
   //GET
-  async review(req, res, next) {}
+  async review(req, res, next) {
+    const listReport = await reports.findAll({
+      where: {
+        type: 'review',
+      },
+    })
+    res.status(200).json({
+      listReport,
+    })
+  }
 }
 
 module.exports = new ReportController()
--- src/modules/report/report.route.js
+++ src/modules/report/report.route.js
@@ -3,7 +3,21 @@ const router = express.Router()
 const reportController = require('./report.controller')
 const { authenToken, restrictTo, permission } = require('../auth/auth.middleware')
 
-router.get('/job', reportController.job)
-router.get('/review', authenToken, restrictTo('Admin', 'Director', 'Hr'), permission('READ'), reportController.review)
+router.get(
+  '/job',
+  authenToken,
+  restrictTo('Admin', 'Director', 'Hr'),
+  permission('READ'),
+  reportController.dataJob,
+  reportController.job
+)
+router.get(
+  '/review',
+  authenToken,
+  restrictTo('Admin', 'Director', 'Hr'),
+  permission('READ'),
+  reportController.dataReview,
+  reportController.review
+)
 
 module.exports = router
